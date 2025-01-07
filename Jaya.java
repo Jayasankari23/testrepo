69e02252e8f30b209ae203f71df76e4823f97bc9
Codoid <jayasankari.m@codoid.com>
2025-01-07 19:27:16 +0530

    Commit through Intellij

---
+++ Jaya.java
@@ -0,0 +1,12 @@
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Jaya {
+    public static void main(String[] args) {
+
+        System.out.println("This is my first project");
+
+    }
+}
